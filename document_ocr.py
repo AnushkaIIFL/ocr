2ba43407e19643d82dd2181459447dc6e53e7791
AnushkaIIFL <140401880+AnushkaIIFL@users.noreply.github.com>
2024-01-24 14:21:56 +0530

    new logger statement in document_ocr.py

--- document_ocr.py
+++ document_ocr.py
@@ -12,6 +12,8 @@ pytesseract.pytesseract.tesseract_cmd = os.getenv('TESSERACT_PATH')
 # pytesseract.pytesseract.tesseract_cmd = r'dependencies/Tesseract-OCR/tesseract.exe'
 
 def extract_text(file,input_path, lang='eng'):
+    logger_msg("Inside extract_text function")
+    logger_msg(f"poppler path: {os.getenv('POPPLER_PATH')}")
     try:  
         if input_path.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
             image = Image.open(file)
@@ -19,6 +21,7 @@ def extract_text(file,input_path, lang='eng'):
         elif input_path.lower().endswith('.pdf'):
             pdf_bytes = file.read()
             images = convert_from_bytes(pdf_bytes, dpi=300, poppler_path= os.getenv('POPPLER_PATH'))
+            
             # images = convert_from_path(temp_path,poppler_path='C:/Users/saurabh/Downloads/Release-23.08.0-0/poppler-23.08.0/Library/bin')
             pdf_text = []
             for page_num, image in enumerate(images, start=1):
